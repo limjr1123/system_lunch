ad0060cd1275bca741df0fce9586d82250285ae9
limjr1123 <63219254+limjr1123@users.noreply.github.com>
2025-05-28 13:36:26 +0900

    Move .gitignore to Unity project root and remove ignored files

---
+++ System_Luncher/Assets/Scripts/Common/SceneLoader.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum SceneType
+{
+    Title,
+    Lobby,
+    InGame,
+}
+
+public class SceneLoader : SingletonBehaviour<SceneLoader>
+{
+    public void LoadScene(SceneType sceneType)
+    {
+        Logger.Log($"Loading scene: {sceneType}");
+
+        Time.timeScale = 1;
+        SceneManager.LoadScene(sceneType.ToString());
+    }
+
+    public void ReloadScene()
+    {
+        Logger.Log($"Loading scene: {SceneManager.GetActiveScene().name}");
+
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
